1e6ef6ecddf6a1cef0b4b83e56e31070b84f147f
PrincipalPageMik <161916454+PrincipalPageMik@users.noreply.github.com>
2024-03-06 07:49:45 -0500

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username === 'Abel' && password === 'Contodosmenoscontigo') {
+    if (username === 'Abel' && password === 'nomebañosoycochina') {
         message.textContent = 'Te quiero mucho<3';
 
         setTimeout(function() {
